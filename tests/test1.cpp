2021d299d4eb8ed85436486f58beea976d20544c
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-05-16 09:39:45 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "../prototype/Prototype.cpp" 
 
-TEST(PrototypeTest, CloneTest) { 
+TEST(PrototypeTest1, CloneTest) { 
 	ConcretePrototypeA prototypeA; 
 	ConcretePrototypeB prototypeB; 
 
@@ -13,4 +13,44 @@ TEST(PrototypeTest, CloneTest) {
 
 	delete clonedA; 
 	delete clonedB; 
+}
+
+TEST(PrototypeTest2, DifferentObjectsTest) {
+    ConcretePrototypeA prototypeA;
+    ConcretePrototypeB prototypeB;
+
+    Prototype* clonedA1 = prototypeA.clone();
+    Prototype* clonedA2 = prototypeA.clone();
+
+    Prototype* clonedB1 = prototypeB.clone();
+    Prototype* clonedB2 = prototypeB.clone();
+
+    // Проверяем, что клонированные объекты различны
+    EXPECT_NE(clonedA1, clonedA2);
+    EXPECT_NE(clonedB1, clonedB2);
+
+    delete clonedA1;
+    delete clonedA2;
+    delete clonedB1;
+    delete clonedB2;
+}
+
+TEST(PrototypeTest3, TypeChangeAfterClone) {
+    // Создаем исходный объект типа ConcretePrototypeA
+    ConcretePrototypeA prototypeA;
+
+    // Клонируем объект
+    Prototype* clonedA = prototypeA.clone();
+
+    // Проверяем, что тип клонированного объекта изменился
+    EXPECT_EQ(clonedA->type(), "type A");
+    
+    // Меняем тип клонированного объекта
+    clonedA->type() = "changed type A";
+
+    // Проверяем, что тип изменился
+    EXPECT_EQ(clonedA->type(), "changed type A");
+
+    // Удаляем клон
+    delete clonedA;
 }
