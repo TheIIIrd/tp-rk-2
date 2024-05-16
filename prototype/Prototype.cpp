cfa53e65dca7b7b7196b3f2d0c7b7280a5fe0185
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-05-16 12:25:13 +0300

    Create Prototype.cpp

---
+++ prototype/Prototype.cpp
@@ -0,0 +1,26 @@
+#include "prototype.hpp"
+#include <iostream>
+
+Prototype* Client::types[2];
+int Client::n_types = 2;
+
+void Client::init()
+{
+  types[0] = new ConcretePrototypeA();
+  types[1] = new ConcretePrototypeB();
+}
+
+void Client::remove()
+{
+  delete types[0];
+  delete types[1];
+}
+
+Prototype* Client::make(const int index)
+{
+  if (index >= n_types)
+  {
+    return nullptr;
+  }
+  return types[index]->clone();
+}
