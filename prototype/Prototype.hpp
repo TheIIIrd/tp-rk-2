d789ec31fe05591bc24c641bcc83cc07a5d487ee
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-05-16 14:34:49 +0300

    Update Prototype.hpp

--- prototype/Prototype.hpp
+++ prototype/Prototype.hpp
@@ -17,14 +17,8 @@ class ConcretePrototypeA : public Prototype
 public:
   ~ConcretePrototypeA() {}
   
-  Prototype* clone() override
-  {
-    return new ConcretePrototypeA();
-  }
-  std::string type() override
-  {
-    return "type A";
-  }
+  Prototype* clone();
+  std::string type();
 };
 
 class ConcretePrototypeB : public Prototype
@@ -32,26 +26,8 @@ class ConcretePrototypeB : public Prototype
 public:
   ~ConcretePrototypeB() {}
   
-  Prototype* clone() override
-  {
-    return new ConcretePrototypeB();
-  }
-  std::string type() override
-  {
-    return "type B";
-  }
+  Prototype* clone();
+  std::string type();
 };
 
-class Client
-{
-public:
-  static void init();
-  static void remove();
-  static Prototype* make(const int index);
-
-private:
-  static Prototype* types[2];
-  static int n_types;
-};
-
-#endif // PROTOTYPE_HPP
+#endif
